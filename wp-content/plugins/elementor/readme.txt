13b87e9ea1862da39898e7e67971f35dd8c4f392
Jeremy <jeremy.teurterie@gmail.com>
2023-09-16 11:04:56 +0700

    :sparkles: Feat: add header

--- wp-content/plugins/elementor/readme.txt
+++ wp-content/plugins/elementor/readme.txt
@@ -4,7 +4,7 @@ Tags: page builder, editor, landing page, drag-and-drop, elementor, visual edito
 Requires at least: 6.0
 Tested up to: 6.3
 Requires PHP: 7.0
-Stable tag: 3.16.1
+Stable tag: 3.16.2
 Beta tag: 3.16.0-beta4
 License: GPLv3
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
@@ -240,6 +240,10 @@ You can also add a new language via [translate.wordpress.org](https://go.element
 == Changelog ==
 
 See Elementor Developer Edition versions changelog [here](https://go.elementor.com/dev-changelog/)
+
+= 3.16.2 - 2023-09-13 =
+
+* Fix: Various issues with deprecation updates and 3rd party plugins
 
 = 3.16.1 - 2023-09-12 =
 
---
+++ wp-content/themes/hello-theme-child-master/header.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * The template for displaying the header
+ *
+ * This is the template that displays all of the <head> section, opens the <body> tag and adds the site's header.
+ *
+ * @package HelloElementor
+ */
+
+if (!defined('ABSPATH')) {
+	exit; // Exit if accessed directly.
+}
+
+$viewport_content = apply_filters('hello_elementor_viewport_content', 'width=device-width, initial-scale=1');
+$enable_skip_link = apply_filters('hello_elementor_enable_skip_link', true);
+$skip_link_url = apply_filters('hello_elementor_skip_link_url', '#content');
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="<?php echo esc_attr($viewport_content); ?>">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<?php wp_body_open(); ?>
+	<header class="nav-bar">
+		<div class='logo'>
+			<img class="logo-icon" alt="" src="http://planty.local/wp-content/uploads/2023/09/logo.png" />
+			<div class="energy-drink">energy drink</div>
+		</div>
+
+		<div class="item-menu">
+			<div class="item1">Nous rencontrer</div>
+			<div class="item2">Admin</div>
+			<div class="btn-nav">
+				<div class="btn-nav-child">
+					<b class="commander">Commander</b>
+				</div>
+			</div>
+		</div>
+	</header>
+</body>
