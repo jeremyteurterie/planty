3f33c699b4228f1b2303e2ac31201727191788fd
Jeremy <jeremy.teurterie@gmail.com>
2023-10-12 17:54:56 +0200

    :bug: Fix: hook fonctionnality header

--- wp-content/themes/hello-theme-child-master/header.php
+++ wp-content/themes/hello-theme-child-master/header.php
@@ -46,6 +46,10 @@ $skip_link_url = apply_filters('hello_elementor_skip_link_url', '#content');
 				));
 				?>
 			</div>
+			<div class="btn-nav">
+				<div class="btn-nav-child">
+					<a class="commander" href="http://planty.local/commander">Commander</a>
+				</div>
+			</div>
 		</div>
-
 	</header>
