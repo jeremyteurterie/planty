be0c62f4dbfd46da3f44ca5f8014f12aea36675f
Jeremy <jeremy.teurterie@gmail.com>
2023-10-14 18:56:52 +0200

    :lipstick: Style: responsive menu

--- wp-content/themes/hello-theme-child-master/functions.php
+++ wp-content/themes/hello-theme-child-master/functions.php
@@ -53,3 +53,11 @@ function add_admin_link($items, $args)
 }
 
 add_filter('wp_nav_menu_items', 'add_admin_link', 10, 2);
+
+// responsive menu
+function set_container_class($args)
+{
+	$args['container_class'] = str_replace(' ', '-', $args['theme_location']) . '-nav';
+	return $args;
+}
+add_filter('wp_nav_menu_args', 'set_container_class');
